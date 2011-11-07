5da8ed784ac06aa957e8b623eb795823c70f3cfc
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2011-11-07 10:35:25 +0100

    po dodaniu skorki

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -1,20 +1,36 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html>
+<html lang="en">
   <head>
+    <meta charset="utf-8" />    
+    <?php include_title() ?>
     <?php include_http_metas() ?>
     <?php include_metas() ?>
-    <?php include_title() ?>
     <link rel="shortcut icon" href="/favicon.ico" />
     <?php include_stylesheets() ?>
     <?php include_javascripts() ?>
   </head>
-  <body>
-      <ol>
-          <li><a href="<?php echo public_path('backend.php') ?>">admin</a></li>
-          <li><a href="<?php echo url_for('clip/index') ?>">clips</a></li>
-          <li><a href="<?php echo url_for('song/index') ?>">songs</a></li>
-          <li><a href="<?php echo url_for('artist/index') ?>">artists</a></li>
-      </ol>
-    <?php echo $sf_content ?>
-  </body>
+<body>
+<header>
+  <div>
+    <h1><a href="<?php echo url_for('@homepage') ?>">Blues<span></span></a></h1>
+    <nav>
+        <ul>
+            <li id="o1"><a href="<?php echo url_for('artist/index') ?>">">artist<span></span></a></li>
+            <li id="o2"><a href="<?php echo url_for('song/index') ?>">songs<span></span></a></li>
+            <li id="o3"><a href="<?php echo url_for('clip/index') ?>">clips<span></span></a></li>
+        </ul>
+    </nav>
+  </div>      
+</header>
+
+<div id="content">
+    <?php echo $sf_content ?>  
+</div>
+
+<footer>
+    &copy;2011 by student <br/>
+    <a href="<?php echo public_path('backend.php') ?>">admin</a>    
+</footer>
+
+</body>
 </html>
