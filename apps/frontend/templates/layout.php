57ab0dc82c509605dfd0dfa5b55e4c78b51a6490
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2011-11-08 13:24:59 +0100

    skin: sprites, sticky footer

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -10,12 +10,21 @@
     <?php include_javascripts() ?>
   </head>
 <body>
+
+
+<div id="content" lang="la">
+    <div class="text">
+        <?php echo $sf_content ?>
+    </div>
+    <div class="push"></div>
+</div>
+
 <header>
   <div>
     <h1><a href="<?php echo url_for('@homepage') ?>">Blues<span></span></a></h1>
     <nav>
         <ul>
-            <li id="o1"><a href="<?php echo url_for('artist/index') ?>">">artist<span></span></a></li>
+            <li id="o1"><a href="<?php echo url_for('artist/index') ?>">artist<span></span></a></li>
             <li id="o2"><a href="<?php echo url_for('song/index') ?>">songs<span></span></a></li>
             <li id="o3"><a href="<?php echo url_for('clip/index') ?>">clips<span></span></a></li>
         </ul>
@@ -23,13 +32,12 @@
   </div>      
 </header>
 
-<div id="content">
-    <?php echo $sf_content ?>  
-</div>
 
 <footer>
-    &copy;2011 by student <br/>
-    <a href="<?php echo public_path('backend.php') ?>">admin</a>    
+    <div>
+        &copy;2011 by <a href="http://gajdaw.pl">gajdaw</a> <br/>
+        <a href="<?php echo public_path('backend.php') ?>">admin</a>
+    </div>
 </footer>
 
 </body>
