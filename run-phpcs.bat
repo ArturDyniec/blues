53eec9eb4da7eeb4b094e1f601ab5e9eb3506439
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2011-11-09 17:38:56 +0100

    Improved FTP exclusions (ftp_exclude.txt)

--- run-phpcs.bat
+++ run-phpcs.bat
@@ -1,1 +1,1 @@
-phpcs  --standard=Symfony  C:\xampp\htdocs\blues --ignore=00-dodatki,nbproject --extensions=php --encoding=utf-8 > phpcs-wynik.txt
+phpcs  --standard=Symfony  C:\xampp\htdocs\blues --ignore=00-dodatki,nbproject --extensions=php --encoding=utf-8 > doc/phpcs-wynik.txt
